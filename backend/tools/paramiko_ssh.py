e3e78001a52f27acf83adbd008c910aa5c15d31f
ysh <1299331829@qq.com>
2024-07-15 17:26:20 +0800

    add

---
+++ backend/tools/paramiko_ssh.py
@@ -0,0 +1,53 @@
+import paramiko
+import time
+
+
+class ParamikoSSH:
+    def __init__(self,hostname,port,username,password) -> None:
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.password = password
+        self.ssh = None
+        self.channel = None
+
+    def connect(self):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(self.hostname,self.port,self.username,self.password)
+        self.channel = self.ssh.invoke_shell()
+
+    def send_command(self,command):
+        if self.channel:
+            self.channel.send(command + '\n')
+        else:
+            raise Exception("没有活跃的channel")
+
+    def read_output(self,sleep_time=1):
+        try:
+            while True:
+                if self.channel.recv_ready():
+                    output = self.channel.recv(1024).decode('utf-8')
+                    print(output,end="")
+                time.sleep(sleep_time)
+        except KeyboardInterrupt:
+            print("用户键盘中断")
+
+    def close(self):
+        if self.channel:
+            self.channel.close()
+        if self.ssh:
+            self.ssh.close()
+
+# ssh -p 46786 root@connect.beijinga.seetacloud.com
+if __name__ == '__main__':
+    hostname = "connect.beijinga.seetacloud.com"
+    port = 46786
+    username = 'root'
+    password = 'peDyj80Rw6ih'
+
+    client = ParamikoSSH(hostname,port,username,password)
+    client.connect()
+    client.send_command('ls')
+    client.read_output()
+    client.close()
